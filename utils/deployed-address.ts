98028e6eff01699df5afc8d0d7fa553c8d1aa93c
thucvv23 van <levanthuc397@gmail.com>
2024-08-15 16:53:14 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0x4570D74ED5A93097FB90E1b259c04A4a8dc8F06B'
+export const ProxyAdmin = '0x04Fceb64D044c1761B543e1A0b3C30992BF658D0'
+export const SWTRImplementation = '0x1C1f0eCE1CC07Dc7506C1d821c82c2710bD71e59'
     
